8e7c1128fa0df35fd4004785599e26a8fbf51c2d
lipengxiang <lipengxiang@beisen.com>
2017-02-04 17:48:54 +0800

    Update:2017-02-04_17:48:54

--- git_push_source.sh
+++ git_push_source.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # 同步静态文件
-hexo g && hexo g
+echo "正在同步静态文件 ..."
+hexo g && hexo d
+
 # 同步源文件
+echo "正在同步源文件 ..."
 nowTime=`date +%F_%H:%M:%S`
 git add -Af *
 git commit -m "Update:$nowTime"
